b986e46274288d591a25060f30378fa27c151b4c
ConorCallanan2003 <conorcallanan03@gmail.com>
2024-04-16 16:26:55 +0100

    Change model position + rotation

--- app/3d/mach-24-payload.tsx
+++ app/3d/mach-24-payload.tsx
@@ -1,22 +1,14 @@
 "use client";
-import { Canvas, useFrame } from "@react-three/fiber";
-import {
-  useGLTF,
-  Environment,
-  OrbitControls,
-  useCursor,
-  TransformControls,
-} from "@react-three/drei";
-import { useState } from "react";
-import THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Environment, OrbitControls } from "@react-three/drei";
 
 function Mach24Payload(props: any) {
   const { scene } = useGLTF("/mach24-payload.glb");
   return (
     <mesh
-      position={[0.05, 0, 0.1]}
-      rotation={[(Math.PI / 2) * 0.7, 0.2, 0]}
-      scale={5}
+      position={[1.5, -1, 0.5]}
+      rotation={[(Math.PI / 2) * 1.1, Math.PI * 2.2, Math.PI * 1.5]}
+      scale={40}
     >
       <primitive object={scene} />
     </mesh>
@@ -26,11 +18,19 @@ function Mach24Payload(props: any) {
 export default function Mach24PayloadAnimation() {
   return (
     <div className="h-[700px] -mx-12 ">
-      <Canvas camera={{ position: [0, -8, 0], fov: 10 }}>
+      <Canvas
+        camera={{
+          position: [0, -8, 0],
+          fov: 55,
+        }}
+      >
         <ambientLight intensity={2} />
         <Mach24Payload />
+        <OrbitControls
+          onChange={(e) => console.log(e?.target.object)}
+          makeDefault
+        />
         <Environment preset="studio" />
-        <OrbitControls makeDefault />
       </Canvas>
     </div>
   );
